c2fe9f8103f55561b6ea4c3206b91219fec793a7
Dariush <59167222+dariusamiri@users.noreply.github.com>
2022-02-03 16:17:31 +0330

    Add files via upload

---
+++ 2/az2/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Zarineh/Desktop/2/az2/test_isim_beh.exe" -prj "C:/Users/Zarineh/Desktop/2/az2/test_beh.prj" "work.test" "work.glbl" 
